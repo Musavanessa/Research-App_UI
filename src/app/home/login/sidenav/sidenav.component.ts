2b90b34392851fede19f41f91e41e4ba4aa49ffc
Katleho R Makhoba <katlehormakhoba@gmail.com>
2022-04-01 16:20:01 +0200

    test

--- src/app/home/login/sidenav/sidenav.component.ts
+++ src/app/home/login/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 //I need to work with services
 import { SidenavService } from 'src/app/services/navs/sidenav.service';
 import { AppComponent } from 'src/app/app.component';
@@ -12,6 +12,11 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class SidenavComponent implements OnInit {
 
+
+  @ViewChild('profileElement') profileElement: any;
+  @ViewChild('projectElement') projectElement: any;
+  @ViewChild('chatBoxElement') chatBoxElement: any;
+
   @Input('isAuthenticated') isAuthenticated: boolean = false;
   @Input('username') username: string = 'user';
   @Input('logout') logout = () => {};
@@ -21,17 +26,58 @@ export class SidenavComponent implements OnInit {
 
   constructor(public sideNavService: SidenavService) { }
   ngOnInit(): void {
-
-
-    this.changeSideNav()
-   
- 
+    this.changeSideNav();
+    this.activateProfileElement();
   }
+
   navProperties =  SidenavService.navProperties;
   changeSideNav()
   {
      this.sideNavService.changeSideNav();
      this.navProperties = SidenavService.navProperties;
   }
+
+  
+ activateProfileElement()
+ {
+   this.profileElement.nativeElement.style.backgroundColor="rgb(0, 132, 255)";
+   this.profileElement.nativeElement.style.color="white";
+   this.deactivateProjectElement();
+   this.deactivateChaboxElement();
+ }
+ deactivateProfileElement()
+ {
+   this.profileElement.nativeElement.style.backgroundColor="white";
+   this.profileElement.nativeElement.style.color="black";
+
+ }
+
+ activateProjectElement()
+ {
+   this.projectElement.nativeElement.style.backgroundColor="rgb(0, 132, 255)";
+   this.projectElement.nativeElement.style.color="white";
+   this.deactivateProfileElement();
+   this.deactivateChaboxElement();
+ }
+
+ deactivateProjectElement()
+ {
+   this.projectElement.nativeElement.style.backgroundColor="white";
+   this.projectElement.nativeElement.style.color="black";
+ }
+
+ activateChatboxElement()
+ {
+   this.chatBoxElement.nativeElement.style.backgroundColor="rgb(0, 132, 255)";
+   this.chatBoxElement.nativeElement.style.color="white";
+   this.deactivateProfileElement();
+   this.deactivateProjectElement()
+ }
+
+ deactivateChaboxElement()
+ {
+   this.chatBoxElement.nativeElement.style.backgroundColor="white";
+   this.chatBoxElement.nativeElement.style.color="black";
+ }
 
 }
